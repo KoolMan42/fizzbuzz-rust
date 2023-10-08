fde62cb93a4354b40b957ab554fbc0c670104118
KoolMan42 <thomasbanks501@gmail.com>
2023-10-07 23:04:50 -0300

    FizzBuzz but with a map

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+use std::collections::HashMap;
+
 fn main() {
     // Numbers from 1 to 25
+    let mut fizzy_map = HashMap::new();
+    fizzy_map.insert(3, "Fizz".to_string());
+    fizzy_map.insert(5, "Buzz".to_string());
     for num in 1..=25 {
-        if num % 3 == 0 && num % 5 == 0 {
-            println!("FizzBuzz");
-        } else if num % 3 == 0 {
-            println!("Fizz");
-        } else if num % 5 == 0 {
-            println!("Buzz");
-        } else {
-            println!("{:?}", num)
+        let mut ans = vec![];
+        for(k,v) in &fizzy_map {
+           if k % num == 0 {
+               ans.push(v.clone());
+           }
         }
+        let ans2 = ans.join("");
+        println!("{:?}", ans2)
     }
 }
