60458d57b6e49055c16f923113d4344a6fd3883b
KoolMan42 <thomasbanks501@gmail.com>
2023-10-07 22:28:45 -0300

    Completed FizzBuzz again

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     // Numbers from 1 to 25
-    for num in 1..26 {
+    for num in 1..=25 {
         if num % 3 == 0 && num % 5 == 0 {
             println!("FizzBuzz");
         } else if num % 3 == 0 {
